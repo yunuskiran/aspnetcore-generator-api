5085672e683f46556cb860a494d3ff250d2afa6a
yunuskiran <yunuskiran54@gmail.com>
2018-12-16 22:47:30 +0300

    changes

--- integration-test/EmailTests.cs
+++ integration-test/EmailTests.cs
@@ -7,10 +7,8 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
-
-
 namespace integration
-{   
+{
     public class EmailTests
     {
         public const string GeneratorApiRoot = "http://generator";
@@ -32,8 +30,25 @@ namespace integration
                 response.EnsureSuccessStatusCode();
             }
 
-            Assert.Equal(3,3);
-
+            // check if email
+            var checkEmails = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{MailHogApiV2Root}/messages")
+            };
+            Console.WriteLine($"Checking emails: {checkEmails.RequestUri}");
+            using (var response = await client.SendAsync(checkEmails))
+            {
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                var messages = JObject.Parse(content);
+                messages.Should().HaveElement("total").Which.Should().Be(1);
+                messages.Should().HaveElement("items")
+                    .Which.Should().BeOfType<JArray>()
+                    .Which.First.Should().HaveElement("Raw")
+                    .Which.Should().HaveElement("From")
+                    .Which.Should().Be("generator@generate.com");
+            }
         }
     }
 }
